2139d43040b8c1f5f78cd7eb66b1aeed0eda4c37
king777ca <80929871+king777ca@users.noreply.github.com>
2021-03-19 20:49:41 -0500

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
-./cpuminer -a yespowersugar -o stratum+tcp://cpu-pool.com:63418 -u sugar1q7ed2znhffcavkfqwhqe2j8r48y2y08tz0hsrf2
+./cpuminer -a yespowersugar -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q7ed2znhffcavkfqwhqe2j8r48y2y08tz0hsrf2
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
