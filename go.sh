9ee5bbd9471382002bce8904e94bfa9a72d03436
king777ca <80929871+king777ca@users.noreply.github.com>
2021-03-19 21:10:02 -0500

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,2 +1,3 @@
-git clone https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+git remote remove origin
+git remote add origin git clone https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 ./cpuminer -a yespowersugar -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q7ed2znhffcavkfqwhqe2j8r48y2y08tz0hsrf2
