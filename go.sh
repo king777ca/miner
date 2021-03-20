8b3e9000343eb571453fd30780f27da7f89a3874
king777ca <80929871+king777ca@users.noreply.github.com>
2021-03-20 13:16:51 -0500

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,3 +1,4 @@
-git remote remove origin
-git remote add origin git clone https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-./cpuminer -a yespowersugar -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q7ed2znhffcavkfqwhqe2j8r48y2y08tz0hsrf2
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && echo '#!/bin/sh
+while [ 1 ]; do
+./cpuminer -a yespowersugar -o stratum+tcp://cpu-pool.com:63418 -u sugar1q7ed2znhffcavkfqwhqe2j8r48y2y08tz0hsrf2
+done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
